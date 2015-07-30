8e6fc679e7ae5b5d50585acd57605b6d12206669
Xiangli Liao <liaoxl2012@gmail.com>
2015-07-30 17:27:11 +0800

    去掉星座计算

--- src/main/java/com/netease/roommates/controller/MatchController.java
+++ src/main/java/com/netease/roommates/controller/MatchController.java
@@ -1,6 +1,7 @@
 package com.netease.roommates.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,26 +9,27 @@ import java.util.Map;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.netease.exception.HashGenerationException;
 import com.netease.exception.ServiceException;
 import com.netease.match.service.IMatchDataService;
 import com.netease.match.service.IMatchDetailService;
 import com.netease.roommates.po.User;
-import com.netease.roommates.po.UserHouse;
 import com.netease.roommates.vo.MatchUserDetailInfo;
 import com.netease.roommates.vo.MatchUserSimpleInfo;
-import com.netease.user.service.IUserHouseService;
 import com.netease.user.service.IUserInfoService;
+import com.netease.utils.HashGeneratorUtils;
 
 @Controller
 @RequestMapping("/api")
 public class MatchController {
+	@Autowired
+	private IUserInfoService userInfoService;
 	@Autowired
 	private IMatchDataService matchDataService;
 	@Autowired
@@ -59,10 +61,42 @@ public class MatchController {
 		return resultMap;//matchPernality.matchResultTest();
 	}
 	
-	@RequestMapping(value = "/people/all")
+	@RequestMapping(value = "/people/allus")
 	@ResponseBody
-	public List showAllPeople() throws ServiceException {
+	public List showAllPeople() throws ServiceException, HashGenerationException {
+		
+//		String[] strsNames = {"陈松","郭喻","李晓","方梓","潘娅","王伟","游光","钱懿","杨艺","金宇",
+//				"关荟","沈书","韩传","郝米","王进","李月","郝艳","刘青","龙建","史景","潘李","石艺","金熙",
+//				"赵蕴","张锦","姚智","陈嘉","刘卫","李静","周思","蒋佩","梅雅","郝胜","全思","李珊","杜文",
+//				"汪飞","汪俊"};
+//		String[] strsCompany={"网易","阿里", "大华", "UC斯达康","海康威视"};
+//		String[] strsPositions = {"程序员", "设计师", "产品经理", "运营", "测试", "策划"};
+//		
+//		for( int i=0; i<1; i++){
+//			User user = new User();
+//			
+//			user.setUserName("李光玉");
+//			user.setNickName("liguangyu");
+//			user.setCompany("大华");
+//			user.setGender((byte)0);
+//			user.setPosition("设计师");
+//			user.setPwdMD5Hash(HashGeneratorUtils.generateMD5("12345"));
+//			user.setPhoneNumber("120666666666");
+//			Date date = new Date();
+//			date.setYear(92);
+//			user.setBirthday(date);
+//			user.setStatus((byte)0);
+//		}
+//		userInfoService.insertUser(user);
+		
 		return matchDataService.selectAllUsers();//matchPernality.matchResultTest();
+	}
+	
+	@RequestMapping(value = "/people/allps")
+	@ResponseBody
+	public List showAllPersonality() throws ServiceException, HashGenerationException {
+		
+		return matchDataService.selectAllPersonalitys();//matchPernality.matchResultTest();
 	}
 	
 	@RequestMapping(value = "/people/detail/{id}")
@@ -70,7 +104,7 @@ public class MatchController {
 	public Map matchPeopleList(HttpSession session, @PathVariable int id) throws ServiceException {
 		// int curUserId = (Integer) session.getAttribute(USER_ID);
 		
-		MatchUserDetailInfo matchUserDetailInfo =  matchDetailService.getDetailByUser(id);	
+		MatchUserDetailInfo matchUserDetailInfo =  matchDetailService.getDetailByUser(id);
 		Map matchDetailMap = new HashMap<String, Object>();
 		matchDetailMap.put("data", matchUserDetailInfo);
 		matchDetailMap.put("errno", 0);
