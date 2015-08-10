373c651592045bda54b0ae52a33206422ac099f5
hzlinqinli <hzlinqinli@corp.netease.com>
2015-08-10 16:46:20 +0800

    add single sign on

--- src/main/java/com/netease/common/service/impl/DefaultActiveMQMailSender.java
+++ src/main/java/com/netease/common/service/impl/DefaultActiveMQMailSender.java
@@ -1,23 +1,35 @@
 package com.netease.common.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
 import com.netease.common.service.ActiveMQMailSender;
 import com.netease.common.service.MailSender;
+import com.netease.exception.ServiceException;
 import com.netease.roommates.vo.MailVO;
 
 @Service
 public class DefaultActiveMQMailSender implements ActiveMQMailSender {
+	private Logger logger = LoggerFactory.getLogger(DefaultActiveMQMailSender.class);
+	
 	@Autowired
 	private MailSender mailSender;
 	
 	@Override
-	//@JmsListener(destination = "mailbox-destination", containerFactory = "jmsListenerContainerFactory")
+	@JmsListener(destination = "mailbox-destination", containerFactory = "jmsListenerContainerFactory")
 	public void send(MailVO mail) {
-		System.out.println("Mail subject: " +mail.getSubject());
-
+		try {
+			mailSender.setReceiver(mail.getReceiver());
+			mailSender.setSubject(mail.getSubject());
+			mailSender.setContent(mail.getContent());
+			mailSender.send();
+			logger.info("Mail has been sent.");
+		} catch (ServiceException e) {
+			logger.error("Error sending email.", e);
+		}
 	}
 
 }
