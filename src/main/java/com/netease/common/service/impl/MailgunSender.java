aa2ce2c26104436021ab29c327af91e4fd902ab4
jiajunwei <hzjiajunwei@corp.netease.com>
2015-08-12 14:42:10 +0800

    加入mailgun

--- src/main/java/com/netease/common/service/impl/MailgunSender.java
+++ src/main/java/com/netease/common/service/impl/MailgunSender.java
@@ -2,17 +2,23 @@ package com.netease.common.service.impl;
 
 import java.io.File;
 
+
+import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.netease.common.service.MailSender;
 import com.netease.exception.ServiceException;
-//import com.sun.jersey.api.client.Client;
-//import com.sun.jersey.api.client.ClientResponse;
-//import com.sun.jersey.api.client.WebResource;
-//import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
-//import com.sun.jersey.multipart.FormDataMultiPart;
-//import com.sun.jersey.multipart.file.FileDataBodyPart;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import com.sun.jersey.multipart.FormDataMultiPart;
 
 public class MailgunSender implements MailSender {
-
+	
+	private Logger logger = LoggerFactory.getLogger(DefaultMailSender.class);
 	
 	private String receiver;
 	private String subject;
@@ -41,22 +47,32 @@ public class MailgunSender implements MailSender {
 
 	@Override
 	public void send() throws ServiceException {
-//		 Client client = Client.create();
-//		 client.addFilter(new HTTPBasicAuthFilter("api",
-//		             "key-5f651d12e3bee58d3c391fd8ca854710"));
-//		 WebResource webResource =
-//		         client.resource("https://api.mailgun.net/v3/mail.jchen.cc/" +
-//		                     "messages");
-//		 FormDataMultiPart form = new FormDataMultiPart();
-//		 form.field("from", "<roommates123@sina.com>");
-//		 form.field("to", receiver);
-//		 form.field("bcc", "bar@example.com");
-//		 form.field("cc", "baz@example.com");
-//		 form.field("subject", "Hello");
-//		 form.field("text", "这是验证邮件，请访问如下网址：<br/><a href=hh> sss </a>");
-//		 form.field("html", "<html>这是验证邮件，请访问如下网址：<br/><a href=hh> sss </a></html>");
-//		System.out.println( webResource.type(MediaType.MULTIPART_FORM_DATA_TYPE).
-//		         post(ClientResponse.class, form) );
+		try{
+		 Client client = Client.create();
+		 client.addFilter(new HTTPBasicAuthFilter("api",
+		             "key-5f651d12e3bee58d3c391fd8ca854710"));
+		 WebResource webResource =
+		         client.resource("https://api.mailgun.net/v3/mail.jchen.cc/" + "messages");
+		 FormDataMultiPart form = new FormDataMultiPart();
+		 form.field("from", "<roommates123@sina.com>");
+		 form.field("to", receiver);
+		 form.field("subject", subject);
+		 form.field("html", content);
+		 webResource.type(MediaType.MULTIPART_FORM_DATA_TYPE).post(ClientResponse.class, form);
+		}
+		catch (Exception mex) {
+			logger.error("Can not send mail to target user:" + receiver);
+			throw new ServiceException("Can not send mail to target user.", mex);
+		} 
+		finally{
+			clearMailStatus();
+		}
 	}
+	private void clearMailStatus() {
+		receiver = "";
+		subject = "";
+		content = "";
+	}
+
 
 }
